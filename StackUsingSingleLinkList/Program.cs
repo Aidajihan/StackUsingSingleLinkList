9e810792135b372ae1d22fa19121d39a048bc9de
TC1045 <aida.jihan.ft21@mail.umy.ac.id>
2022-11-28 16:46:31 +0700

    add stack menu

--- StackUsingSingleLinkList/Program.cs
+++ StackUsingSingleLinkList/Program.cs
@@ -50,19 +50,36 @@ namespace StackUsingSingleLinkList
 
         public void display()
         {
-            node top;
+            node tmp;
             if (empty())
                 Console.WriteLine("\nStack Empty");
             else
             {
+                for (tmp = top; tmp != null; tmp = tmp.next)
+                {
+                    Console.WriteLine(tmp.info);
+                }
+            }
+        }
+    }
+    class progam
+    {
+        static void Main(string[] args)
+        {
+            Stack s = new Stack();
+            while (true)
+            {
+                Console.WriteLine();
+                Console.WriteLine("\n***Stack Menu***\n");
+                Console.WriteLine("1. Push");
+                Console.WriteLine("2. Pop");
+                Console.WriteLine("3. Display");
+                Console.WriteLine("4. Exit");
+                Console.Write("\nEnter your choice: ");
 
             }
         }
     }
-
-
-    static void Main(string[] args)
-    {
-    }
+    
 }
 
