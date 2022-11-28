c951e3c51f11a8ad59824f298feb189d8e078c0d
TC1045 <aida.jihan.ft21@mail.umy.ac.id>
2022-11-28 16:53:40 +0700

    add case 2

--- StackUsingSingleLinkList/Program.cs
+++ StackUsingSingleLinkList/Program.cs
@@ -85,6 +85,15 @@ namespace StackUsingSingleLinkList
                         int num = Convert.ToInt32(Console.ReadLine());
                         s.push(num);
                         break;
+
+                    case '2':
+                        if(s.empty())
+                        {
+                            Console.WriteLine("\nStack Empty");
+                            break;
+                        }
+                        s.pop();
+                        break;
                 }
 
             }
