2f24c8b3b1c686944d90479222d27bbf75a038da
TC1045 <aida.jihan.ft21@mail.umy.ac.id>
2022-11-28 16:22:23 +0700

    Add project files.

---
+++ StackUsingSingleLinkList/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackUsingSingleLinkList
+{
+    class node
+    {
+        public int info;
+        public node next;
+        public node(int i,node n)
+        {
+
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
